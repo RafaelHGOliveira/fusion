c92dd598dc94ee0491536b9086c155e1feb60e47
Rafael Henrique <rafaelh9six@hotmail.com>
2022-07-05 23:07:44 -0300

    Criado view IndexView

--- fusion/urls.py
+++ fusion/urls.py
@@ -17,7 +17,7 @@ from pathlib import Path
 from django.contrib import admin
 from django.urls import path, include
 
-from django.conf.urls import static
+from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
