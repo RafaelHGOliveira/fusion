b4dbf21ec68a8c71b38ff47ae9687781ebb05880
Rafael Henrique <rafaelh9six@hotmail.com>
2022-07-06 20:38:44 -0300

    Criado model serviço

--- core/models.py
+++ core/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from stdimage.models import StdImageField
 
-# Create your models here.
+
+class Base(models.Model):
+    criados = models.DateField("Criação", auto_now_add=True)
+    modificado = models.DateField("Atualização", auto_now=True)
+    ativo = models.BooleanField("Atuvi", default=True)
+    
+    class Meta:
+        abstract = True
+        
+class Servico(Base):
+    ICONE_CHOISES = (
+        ('lni-cog', 'Engrenagem'),
+        ('lni-stats-up', 'Grafico'),
+        ('lni-users', 'Usuarios'),
+        ('lni-layers', 'Design'),
+        ('lni-mobile', 'Mobile'),
+        ('lni-rocket', 'Foguete'),
+    )
+    servico = models.CharField('Servico', max_length=100)
+    descricao = models.TextField('Descrição', max_length=200)
+    icone = models.CharField('Icone', max_length=12, choices=ICONE_CHOISES)
+    
+    class Meta:
+        verbose_name = "Serviço"
+        verbose_plural = "Serviços"
+        
+    def __str__(self):
+        return self.servico
--- fusion/settings.py
+++ fusion/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 STATIC_ROOT = Path(BASE_DIR, "staticfiles")
 MEDIA_ROOT = Path(BASE_DIR, "media")
 
