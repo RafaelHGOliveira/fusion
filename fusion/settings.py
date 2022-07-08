ae4a1d1aaf29d2a4d6590a62e3807f93407a8ce5
Rafael Henrique <rafaelh9six@hotmail.com>
2022-07-08 00:57:42 -0300

    Voltando configurações de database produção

--- fusion/settings.py
+++ fusion/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'fusion.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -91,12 +91,12 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
 """
+
 DATABASES = {
     'default': dj_database_url.config()
 }
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
